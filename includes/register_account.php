5064a5dce1245136d26b6b0cc06f10fc9c634731
gcb011 <117048354+gcb011@users.noreply.github.com>
2022-11-05 08:15:46 -0400

    Add files via upload
    Change params in includes/connect.php to your stuff. Shouldn't need a port number.
    
    Goes directly into htdocs (sorry!). We can fix directories/paths whenever. Registration/login works with users table (add a varchar(128) non-null PWD column to users to hold the hashed password created by the PHP). Your encryption method will be way better of course, but I just wanted something that works for now. 
    
    All the error messages work in register.php and login.php (the included webflow.js prevents empty fields, so I couldn't actually test that error in register.php but I'm sure it works). Successful login redirects back to the index page for now (didn't test if login works for Email too, it should).
    
    Some of the original HTML docs probably need to be changed along with their links. Paths are a mess rn, but the important stuff works lol. I'll try to clean up some more when I can.

---
+++ includes/register_account.php
@@ -0,0 +1,52 @@
+<?php
+	if (isset($_POST['submit'])) {
+	
+		$F_Name = $_POST['F_Name'];
+		$L_Name = $_POST['L_Name'];
+		$Email = $_POST['Email'];
+		$PWD = $_POST['PWD'];
+		$cPWD = $_POST['cPWD'];
+		$displayname = $_POST['displayname'];
+		
+		require_once 'connect.php';
+		require_once 'functions.php';
+		
+//Error messages
+
+	//Check for empty fields
+		if (emptyInputSignup($F_Name, $L_Name, $Email, $displayname, $PWD, $cPWD) !== false) {
+			header("location: ../register.php?error=emptyinput");
+			exit();
+		}
+	//Invalid username/email checks	
+		if (invalidUname($displayname) !== false) {
+			header("location: ../register.php?error=invalidUname");
+			exit();
+		}
+		if (invalidEmail($Email) !== false) {
+			header("location: ../register.php?error=invalidEmail");
+			exit();
+		}
+	// Check PWD length
+		if (pwdLength($PWD) !== false) {
+			header("location: ../register.php?error=passwordTooshort");
+			exit();
+		}
+			
+	//Check for matching password fields	
+		if (pwdConfirm($PWD, $cPWD) !== false) {
+			header("location: ../register.php?error=passwordMismatch");
+			exit();
+		}
+	//Check for exisitng username
+		if (uidExists($conn, $displayname) !== false) {
+			header("location: ../register.php?error=usernametaken");
+			exit();
+		}
+
+//Registration 
+		registerUser($conn, $F_Name, $L_Name, $Email, $displayname, $PWD);
+	}	else {
+		header("location: ../register.php");
+		exit();
+}
